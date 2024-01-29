adeeea16ed8442b76e9f01199df9202c4162bf87
Rafael Morales <rmorales@utem.cl>
2024-01-28 21:07:22 -0300

    Configuraciones de Hyperland

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -1,14 +1,80 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  menu = "wofi --show drun";
+in
 {
+  home.packages = with pkgs; [
+    polkit
+    polkit_gnome
+    wofi
+    mako
+  ];
+
   wayland.windowManager.hyprland = {
     enable = true;
 
     settings = {
       "$mod" = "SUPER";
-      bind =
-        [
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 20;
+        border_size = 2;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+
+        layout = "dwindle";
+      };
+
+      decoration = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+        rounding = 10;
+
+        blur = {
+            enabled = true;
+            size = 3;
+            passes = 1;
+            
+            vibrancy = 0.1696;
+        };
+
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
+
+      animations = {
+        enabled = true;
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+      animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      bind = [
           "$mod, Return, exec, alacritty"
+          "$mod, Q, exec, killactive"
+          "$mod, M, exit"
+          "$mod, D, exec, ${menu}"
+
+          # Focus windows
+          "$mod, H, movefocus, l"
+          "$mod, J, movefocus, d"
+          "$mod, K, movefocus, u"
+          "$mod, L, movefocus, r"
+
+          # Mover ventana usando Super
+          "$mod, mouse:272, movewindow"
         ]
         ++ (
           # workspaces
@@ -22,6 +88,8 @@
               in [
                 "$mod, ${ws}, workspace, ${toString (x + 1)}"
                 "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+                # "$mod, KP_${ws}, workspace, ${toString (x + 1)}"
+                # "$mod SHIFT, KP_${ws}, movetoworkspace, ${toString (x + 1)}"
               ]
             )
             10)
